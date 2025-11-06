8b2bd3660e955386ae2d93a290d4cbd08a18d412
tutsi193 <tutsi.nc@outlook.com>
2025-11-06 22:33:43 +0100

    first commit

--- src/h3/H3_Main.java
+++ src/h3/H3_Main.java
@@ -2,6 +2,37 @@ package h3;
 
 public class H3_Main {
 	public static void main(String[] args) {
+		int max = 100;
+		int fix = 50;
+		int wartend = 80;
+		boolean istVoll;
 		
+		istVoll = (fix == max);
+		
+		if (fix < max && wartend > 0) {
+			fix = fix + wartend;
+			if (fix > max) {
+				wartend = fix - max;
+				fix = max;
+				istVoll = true;
+			}else {
+				wartend = 0;
+				istVoll = false;
+			}
+		}
+		
+		System.out.println("max= "+max);
+		System.out.println("fix= "+fix);
+		System.out.println("wartend= "+wartend);
+		System.out.println(istVoll);
 	}
 }
+
+/*echo "# Uebung3" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/tutsi193/Uebung3.git
+git push -u origin main
+*/
